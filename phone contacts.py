58debbb58dd2407b80d4011fa45178ed39f0610f
Disys-Vidhyavijyabaskar <95480655+Disys-Vidhyavijyabaskar@users.noreply.github.com>
2021-12-15 20:30:43 +0530

    Add files via upload
    rectified phone contacts

---
+++ phone contacts.py
@@ -0,0 +1,50 @@
+class phonecontacts():
+
+    def __init__(self):
+
+        self.__name=input("enter your name")
+
+        self.__type=input("enter type-press 1 for existing")
+        
+
+        if (self.__type)=="1":
+            
+            print("existing contacts")
+
+        elif print("new contact"):
+            return
+        
+
+    def number(self):
+            
+        self.__number=input("enter the phone number")
+
+        if (self.__number,str):
+            print("accept")
+
+        else:
+            print("write a correct number")
+
+    def information(self):
+
+        self.__email=input("enter your email id")
+        print(self.__email)
+
+        self.__favourites=input("to add as favourites?- press 1 to accept")
+
+        if(self.__favourites)=="1":
+            print("added to favourites")
+
+        elif print("option denied"):
+            return
+
+
+
+
+
+user=phonecontacts()
+user.number()
+user.information()
+        
+                  
+        
